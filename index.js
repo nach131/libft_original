dc854bec8739280b6d3676a0b4f6e94c86a58bb2
nacho <nacho.mota@gmail.com>
2022-08-03 16:54:15 +0200

    cositas

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs').promises
 const axios = require('axios');
-
-
+require('dotenv').config()
 
 async function getData(url){
 
@@ -9,8 +8,7 @@ async function getData(url){
 		method: 'get',
 		url: url,
 		headers: { 
-			// 'Authorization': 'Basic bmFjaDEzMTpnaHBfY3hMT0E2NFM5TGtqZURIV1lWUzFSWkh1UENZbGFLMFc0RGJ0'
-			'Authorization': '${{ secrets.AUTH }}'
+			'Authorization': process.env.SECRET_KEY
 		}
 	};
 	const res = await axios(config)
@@ -18,11 +16,6 @@ async function getData(url){
 	// console.log(res.data.count)
   return (res.data.count);
 }
-
-console.log('${{ secrets.AUTH }}')
-console.log(`${{ secrets.AUTH }}`)
-
-// const TOTAL = "%{{total_views}}%"
 
 (async()=>{
 	const views = await getData('https://api.github.com/repos/nach131/libft/traffic/views');
